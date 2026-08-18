c9a6012b96b307dd78d44a528b9a4c22413983aa
Lex Ferguson <82519402+SynisterSage@users.noreply.github.com>
2026-08-18 00:02:39 -0400

    fix: idk

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,5 +1,5 @@
 window.__LEDGER_RUNTIME__ = {
   apiUrl: 'https://api.ledgerworkspace.com',
   supabaseUrl: 'https://cocaffekowzejftiehql.supabase.co',
-  supabasePublishableKey: 'sb_publishable_Bc0hVe5AIdEpCCpXsTDMfg_tImeYQ_S',
+  supabasePublishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvY2FmZmVrb3d6ZWpmdGllaHFsIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3Nzc1ODc0NDcsImV4cCI6MjA5MzE2MzQ0N30.525IRtHYBv99m8njaFPXlKQRdq045Df_0v5-6SYxOxQ',
 }
